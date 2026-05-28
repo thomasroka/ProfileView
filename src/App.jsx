d9590bcb889a9948662d36bbfa11cd5eb585d744
Thomas Roka <thomasroka360@gmail.com>
2026-05-28 08:25:40 +0545

    add- make profile component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Profile from './Components/Profile'
+import ProfileContent from './Components/ProfileContent'
+function App() {
+  return(
+    <>
+    <div className='m-16'>
+      <ProfileContent/>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
